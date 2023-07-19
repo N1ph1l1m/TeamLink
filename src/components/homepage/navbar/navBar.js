50b6a633101b76e0bb4e3e99ce527b702396206a
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-19 23:36:36 +0300

    Update navBar.js/css and homePage.css. Create directory img

--- src/components/homepage/navbar/navBar.js
+++ src/components/homepage/navbar/navBar.js
@@ -1,13 +1,33 @@
-import React, {Component} from "react";
-import  "./navBar.css"
+import React, { Component } from "react";
+import "./navBar.css";
+import logo from "./img/logoUser.png";
 
-
-export default class NavBar extends Component{
-   render(){
-    return(
+export default class NavBar extends Component {
+  render() {
+    return (
       <div className="navBar_wrap">
-          <h1>NavBar</h1>
+        <img className="navBar_userLogo" src={logo} alt="Logo" />
+        <div className="navBar_list_wrap">
+          <a href="fff" className="navBrar_list_item">
+            Моя страница
+          </a>
+          <a href="fff" className="navBrar_list_item">
+            Новости
+          </a>
+          <a href="fff" className="navBrar_list_item">
+            Сообщения
+          </a>
+          <a href="fff" className="navBrar_list_item">
+            Группы
+          </a>
+          <a href="fff" className="navBrar_list_item">
+            Задачи
+          </a>
+          <a href="fff" className="navBrar_list_item">
+            Уведомления
+          </a>
+        </div>
       </div>
-    )
-   }
+    );
+  }
 }
