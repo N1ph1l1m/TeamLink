cdfd361380f56d0b08e22199fbe8b7766a65d3dc
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-29 21:42:50 +0300

    Add img for post-list-item.Create layout for component post-list-item

--- src/components/homepage/news/post-list/post-list.js
+++ src/components/homepage/news/post-list/post-list.js
@@ -1,11 +1,12 @@
 import React , {Component}from "react";
 import './post-list.css'
+import PostListItem from "../post-list-item/post-list-item";
 
 export default class Postlist extends Component{
     render(){
         return(
             <div className="postList_wrapper">
-            
+                <PostListItem/>
             </div>
         )
     }
